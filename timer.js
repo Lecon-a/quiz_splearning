b0a6bbb360ca2d9f9a5f013f2e18948c5967fcf3
Sunday P. Afolabi <spog4true@gmail.com>
2022-10-15 09:25:30 -0400

    Timer Event: Reset time

--- timer.js
+++ timer.js
@@ -28,16 +28,18 @@ function minute(m) {
 	min += m;
 	if (min <= 10) {
 		minutes.innerHTML = min;
-	} else {
-		resetTime()
 	}
 }
 
 function resetTime() {
 	if (min === 10) {
+		miniseconds = 0;
 		counter = 0;
 		min = 0;
 	}	
+
+	seconds.innerHTML = counter;
+	minutes.innerHTML = min
 }
 
 setInterval(time, 10); // change 10 to 1000 later
